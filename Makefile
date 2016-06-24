e6f06d0b607f320a7b11797bbc534055ae276849
Noralf Trønnes <noralf@tronnes.org>
2016-06-24 22:41:58 +0200

    Remove lcdreg

--- Makefile
+++ Makefile
@@ -1,7 +1,6 @@
 ccflags-y += -I$(src)/include
 
 obj-$(CONFIG_DRM_TINYDRM)		+= core/
-obj-$(CONFIG_LCDREG)			+= lcdreg/
 
 # Controllers
 obj-$(CONFIG_TINYDRM_MIPI_DBI)		+= mipi-dbi.o
--- lcdreg/internal.h
+++
@@ -1,8 +0,0 @@
-
-#ifdef CONFIG_DEBUG_FS
-void lcdreg_debugfs_init(struct lcdreg *reg);
-void lcdreg_debugfs_exit(struct lcdreg *reg);
-#else
-static inline void lcdreg_debugfs_init(struct lcdreg *reg) { }
-static inline void lcdreg_debugfs_exit(struct lcdreg *reg) { }
-#endif
