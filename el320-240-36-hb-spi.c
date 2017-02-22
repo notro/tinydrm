4c5d9d2d25e09e1984e00bd6b5f46453e121c0a6
Noralf Trønnes <noralf@tronnes.org>
2017-02-22 13:21:10 +0100

    el320-240-36-hb-spi: Add copyrigth

--- el320-240-36-hb-spi.c
+++ el320-240-36-hb-spi.c
@@ -3,6 +3,9 @@
  *
  * Copyright 2017 Noralf Trønnes
  *
+ * RGB565 to monochrome conversion code is taken from fb_agm1264k-fl.c
+ * Copyright (C) 2014 ololoshka2871
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
